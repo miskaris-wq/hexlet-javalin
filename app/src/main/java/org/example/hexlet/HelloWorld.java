2de4b7708ebeace9acc1917054ee77ec224e4a20
Ксения Рассказова <krasskazova@test.ru>
2025-04-23 00:33:02 +0300

    add jte

--- app/src/main/java/org/example/hexlet/HelloWorld.java
+++ app/src/main/java/org/example/hexlet/HelloWorld.java
@@ -5,6 +5,7 @@ import io.javalin.rendering.template.JavalinJte;
 import static io.javalin.rendering.template.TemplateUtil.model;
 import org.example.hexlet.model.Course;
 import org.example.hexlet.dto.courses.CoursesPage;
+
 import java.util.List;
 
 public class HelloWorld {
@@ -14,26 +15,21 @@ public class HelloWorld {
             config.fileRenderer(new JavalinJte());
         });
 
-        // Список курсов
         app.get("/courses", ctx -> {
             var courses = List.of(
-                    new Course(1L, "Основы Java", "Изучение базового синтаксиса Java"),
-                    new Course(2L, "Веб-разработка", "Создание веб-приложений на Java")
+                    new Course(1L, "course1", "test1"),
+                    new Course(2L, "course2", "test2")
             );
-            var page = new CoursesPage(courses, "Наши курсы");
+            var header = "Курсы по программированию";
+            var page = new CoursesPage(courses, header);
             ctx.render("courses/index.jte", model("page", page));
         });
 
-        // Страница курса
         app.get("/courses/{id}", ctx -> {
-            try {
-                long id = Long.parseLong(ctx.pathParam("id"));
-                // Имитация поиска курса
-                var course = new Course(id, "Курс " + id, "Описание курса " + id);
-                ctx.render("courses/show.jte", model("course", course));
-            } catch (NumberFormatException e) {
-                ctx.status(400).result("Неверный ID курса");
-            }
+            var id = Long.parseLong(ctx.pathParam("id"));
+            // В реальном приложении здесь будет поиск в базе данных
+            var course = new Course(id, "course" + id, "description for course " + id);
+            ctx.render("courses/show.jte", model("course", course));
         });
 
         app.start(7070);
--- app/src/main/java/org/example/hexlet/model/Course.java
+++ app/src/main/java/org/example/hexlet/model/Course.java
@@ -1,18 +1,21 @@
 package org.example.hexlet.model;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
 public final class Course {
-    private final Long id;
-    private final String name;
-    private final String description;
+    private Long id;
+    @ToString.Include
+    private String name;
+    private String description;
 
     public Course(Long id, String name, String description) {
         this.id = id;
         this.name = name;
         this.description = description;
     }
-
-    // Геттеры
-    public Long getId() { return id; }
-    public String getName() { return name; }
-    public String getDescription() { return description; }
 }
