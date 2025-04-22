343a087ddfd9d56a3ae6790c103f6242b26e596e
Ксения Рассказова <krasskazova@test.ru>
2025-04-22 21:15:10 +0300

    dynamic m

--- app/src/main/java/org/example/hexlet/HelloWorld.java
+++ app/src/main/java/org/example/hexlet/HelloWorld.java
@@ -7,13 +7,20 @@ public class HelloWorld {
         var app = Javalin.create(config -> {
             config.bundledPlugins.enableDevLogging();
         });
-        app.get("/users", ctx -> ctx.result("GET /users"));
+        app.get("/users/{id}/post/{postId}", ctx -> {
+            var id = ctx.pathParam("id");
+            var postId =  ctx.pathParam("postId");
+            ctx.result("User ID: " + id + " Post ID: " + postId);
+        });
+        /*app.get("/users", ctx -> ctx.result("GET /users"));
         app.get("/hello", ctx -> {
             String name = ctx.queryParam("name");
             String greeting = name != null ? "Hello, " + name + "!" : "Hello, World!";
             ctx.result(greeting);
         });
         app.post("/users", ctx -> ctx.result("POST /users"));
+
+         */
         app.start(7070);
     }
 }
